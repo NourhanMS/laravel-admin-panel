8d7ab18a13e8922d592d832ffea605adc4780763
NourhanMS <nourhan_195@hotmail.com>
2019-09-23 17:49:59 +0200

    modify

--- database/migrations/2019_09_14_100218_create_agents_table.php
+++ database/migrations/2019_09_14_100218_create_agents_table.php
@@ -19,6 +19,7 @@ class CreateAgentTable extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->integer('phone');
+            $table->binary('admin');
         
         });
     }
